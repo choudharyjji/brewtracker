1b6c74394b5ff2f1936d63036ff5563ff418856c
Ray Blick <rblick.ecol@gmail.com>
2018-07-10 19:54:39 +1000

    day 2 - added button and state to RootContainer

--- Containers/RootContainer.js
+++ Containers/RootContainer.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react'
 import { 
-    View, 
-    Text } from 'react-native'
+  TouchableOpacity,
+  View, 
+  Text } from 'react-native'
 import styles from './Styles/RootContainerStyles'
 
 export default class RootContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { test: "Testing" }
+  }
+
+  pressMe = () => {
+    if (this.state.test == "Testing") {
+      this.setState({
+        test: "DONE"
+      })
+    } else {
+      this.setState({
+        test: "Testing"
+      })     
+    }
+  }
+
   render () {
     return (
       <View style={styles.container}>
+
+        <TouchableOpacity style={styles.button}
+          onPress={this.pressMe}>
+
+          <Text style={styles.welcome}> 
+            Press me! 
+          </Text>
+
+        </TouchableOpacity>
+
         <Text style={styles.welcome}>
-            Testing
+            {this.state.test}
         </Text>
+
       </View>
     )
   }
--- Containers/Styles/RootContainerStyles.js
+++ Containers/Styles/RootContainerStyles.js
@@ -5,11 +5,23 @@ import {
 export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    color: 'red'    
+    color: 'purple'    
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 200,
+    height: 50,
+    backgroundColor: 'pink',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'red'
   }
 })
