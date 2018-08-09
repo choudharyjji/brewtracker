f51426c078b3ce597fa2fed1860f260460fd82f6
Ray Blick <rblick.ecol@gmail.com>
2018-08-09 19:47:46 +1000

    Day 26 - added data

---
+++ Data/grains.js
@@ -0,0 +1,517 @@
+export default [
+    {  
+      name: "Pale Base Malts",
+      id: 1,
+      data: [{
+          name: "Castle Pale Ale Malted Barley",
+          id: 101,
+          color: 'light',
+          usage: ['Pale Ale', 'Bitter']
+        },{
+          name: "Castle Pilsener Malted Barley",
+          id: 102,
+          color: 'light',
+          usage: ['Pale Ale', 'All']
+        },{
+          name: "Crisp Best Ale Malt",
+          id: 103,
+          color: 'light',
+          usage: ['Pale Ale', 'All']
+        },{
+          name: "Crisp Chevallier Heritage Malt",
+          id: 104,
+          color: 'light',
+          usage: ['Ale']
+        },{
+          name: "Crisp Maris Otter Ale Malt",
+          id: 105,
+          color: 'light',
+          usage: ['Ale']
+        },{
+          name: "Gladfield Ale Malt",
+          id: 106,
+          color: 'light',
+          usage: ['Dark Ale']
+        },{
+          name: "Gladfield American Ale Malt",
+          id: 107,
+        },{
+          name: "Gladfield Pilsener Malt",
+          id: 108,
+        },{
+          name: "Joe White Export Pilsner Malt",
+          id: 109,
+        },{
+          name: "Joe White Traditional Ale",
+          id: 110,
+        },{
+          name: "Munton's Lager Malt",
+          id: 111,
+        },{
+          name: "Munton's Maris Otter",
+          id: 112,
+        },{
+          name: "Munton's Mild Ale Malt",
+          id: 113,
+        },{
+          name: "Munton's Pale Ale Malt",
+          id: 114,
+        },{
+          name: "Munton's Pilsner Malt",
+          id: 115,
+        },{
+          name: "Thomas Fawcett Floor Malted Golden Promise",
+          id: 116,
+        },{
+          name: "Thomas Fawcett Floor Malted Maris Otter",
+          id: 117,
+        },{
+          name: "Weyermann Pilsener Malt",
+          id: 118,
+        }]
+    },
+    {
+      name: "Dark Base Malts",
+      id: 2,
+      data: [{
+          name: "Castle Munich 15 (Light) Malted",
+          id: 201,
+        },{
+          name: "Castle Munich 25 (Dark) Malted",
+          id: 202,
+        },{
+          name: "Joe White Dark Munich",
+          id: 203,
+        },{
+          name: "Joe White Munich Malt",
+          id: 204,
+        },{
+          name: "Joe White Vienna Malt",
+          id: 205,
+        },{
+          name: "Munton's Munich Malt",
+          id: 206,
+        },{
+          name: "Weyermann Dark Munich 2",
+          id: 207,
+        },{
+          name: "Weyermann Light Munich 1",
+          id: 208,
+        },{
+          name: "Weyermann Vienna",
+          id: 209,
+        }]
+    },
+    {
+      name: "Caramel/Crystal Malts",
+      id: 3,
+      data: [{
+          name: "Castle CaraBlond (Cara20) Malted Barley",
+          id: 301,
+        },{
+          name: "Castle CaraRuby (Cara50) Malted Barley",
+          id: 302,
+        },{
+          name: "Castle Crystal Malted",
+          id: 303,
+        },{
+          name: "Castle Special B Malted",
+          id: 304,
+        },{
+          name: "Crisp Caramalt",
+          id: 305,
+        },{
+          name: "Crisp Dark Crystal (Crystal 400)",
+          id: 306,
+        },{
+          name: "Crisp Extra Light Crystal",
+          id: 307,
+        },{
+          name: "Crisp Light Crystal (Crystal 150)",
+          id: 308,
+        },{
+          name: "Crisp Medium Crystal (Crystal 240)",
+          id: 309,
+        },{
+          name: "Gladfield Red Back Malt",
+          id: 310,
+        },{
+          name: "Gladfieldn Toffee Malt",
+          id: 311,
+        },{
+          name: "Joe White Cara Malt",
+          id: 312,
+        },{
+          name: "Joe White Crystal Malt",
+          id: 313,
+        },{
+          name: "Joe White Dark Crystal",
+          id: 314,
+        },{
+          name: "Munton's Cara Malt",
+          id: 315,
+        },{
+          name: "Munton's Dark Crystal Malt",
+          id: 316,
+        },{
+          name: "Simpsons Dark Crystal",
+          id: 317,
+        },{
+          name: "Simpsons Medium Crystal",
+          id: 318,
+        },{
+          name: "Simpsons Pale Crystal",
+          id: 319,
+        },{
+          name: "Weyermann CaraAmber",
+          id: 320,
+        },{
+          name: "Weyermann CaraAroma",
+          id: 321,
+        },{
+          name: "Weyermann Carahell",
+          id: 322,
+        },{
+          name: "Weyermann Caramunich 1",
+          id: 323,
+        },{
+          name: "Weyermann Caramunich 2",
+          id: 324,
+        },{
+          name: "Weyermann Caramunich 3",
+          id: 325,
+        },{
+          name: "Weyermann Carapils Carafoam",
+          id: 326,
+        },{
+          name: "Weyermann Carared",
+          id: 327,
+        }]
+    },
+    {
+      name: "Roasted Malts",
+      id: 4,
+      data: [{
+        name: "Castle Black of Black Malted Barley",
+        id: 401,
+      },{
+        name: "Castle Cafe Light (Coffee) Malted Barley",
+        id: 402,
+      },{
+        name: "Castle Chocolate Malted Barley",
+        id: 403,
+      },{
+        name: "Castle Roasted Barley",
+        id: 404,
+      },{
+        name: "Crisp Amber Malt",
+        id: 405,
+      },{
+        name: "Crisp Brown Malt",
+        id: 406,
+      },{
+        name: "Crisp Chocolate",
+        id: 407,
+      },{
+        name: "Crisp Pale Chocolate",
+        id: 408,
+      },{
+        name: "Gladfield Brown Malt",
+        id: 409,
+      },{
+        name: "Gladfield Shepherds Delight Malt",
+        id: 410,
+      },{
+        name: "Joe White Chocolate Malt",
+        id: 411,
+      },{
+        name: "Joe White Roasted Barley",
+        id: 412,
+      },{
+        name: "Joe White Roasted Malt",
+        id: 413,
+      },{
+        name: "Joe White Roasted Wheat Malt",
+        id: 414,
+      },{
+        name: "Munton's Chocolate Malt",
+        id: 415,
+      },{
+        name: "Munton's Roast Barley",
+        id: 416,
+      },{
+        name: "Simpsons Brown Malt",
+        id: 417,
+      },{
+        name: "Weyermann Carafa Special Type 1",
+        id: 418,
+      },{
+        name: "Weyermann Carafa Special Type 2",
+        id: 419,
+      },{
+        name: "Weyermann Carafa Special Type 3",
+        id: 420,
+      }]
+    },
+    {
+      name: "Wheat, Rye, Other Grains",
+      id: 5,
+      data: [{
+        name: "Castle Crystal Wheat",
+        id: 501,
+      },{
+        name: "Castle Munich Wheat Malt 15",
+        id: 502,
+      },{
+        name: "Castle Munich Wheat Malt 25",
+        id: 503,
+      },{
+        name: "Castle Oat Malt",
+        id: 504,
+      },{
+        name: "Castle Rye Malt",
+        id: 505,
+      },{
+        name: "Castle Spelt",
+        id: 506,
+      },{
+        name: "Castle Wheat Blanc (Pale) Malt",
+        id: 507,
+      },{
+        name: "Crisp Flaked Maize",
+        id: 508,
+      },{
+        name: "Crisp Flaked Torrefied Oats",
+        id: 509,
+      },{
+        name: "Crisp Flaked Torrefied Rice",
+        id: 510,
+      },{
+        name: "Crisp Torrefied Wheat",
+        id: 511,
+      },{
+        name: "Joe White Crystal Wheat Malt",
+        id: 512,
+      },{
+        name: "Joe White Roasted Wheat Malt",
+        id: 513,
+      },{
+        name: "Joe White Wheat Malt",
+        id: 514,
+      },{
+        name: "Munton's Wheat Malt",
+        id: 515,
+      },{
+        name: "Simpsons Golden Naked Oats",
+        id: 516,
+      },{
+        name: "Weyermann Pale Wheat",
+        id: 517,
+      },{
+        name: "Weyermann Rye Malt",
+        id: 518,
+      }]
+    },{
+      name: "Wheat, Rye, Other Grains",
+      id: 5,
+      data: [{
+        name: "Castle Crystal Wheat",
+        id: 501,
+      },{
+        name: "Castle Munich Wheat Malt 15",
+        id: 502,
+      },{
+        name: "Castle Munich Wheat Malt 25",
+        id: 503,
+      },{
+        name: "Castle Oat Malt",
+        id: 504,
+      },{
+        name: "Castle Rye Malt",
+        id: 505,
+      },{
+        name: "Castle Spelt",
+        id: 506,
+      },{
+        name: "Castle Wheat Blanc (Pale) Malt",
+        id: 507,
+      },{
+        name: "Crisp Flaked Maize",
+        id: 508,
+      },{
+        name: "Crisp Flaked Torrefied Oats",
+        id: 509,
+      },{
+        name: "Crisp Flaked Torrefied Rice",
+        id: 510,
+      },{
+        name: "Crisp Torrefied Wheat",
+        id: 511,
+      },{
+        name: "Joe White Crystal Wheat Malt",
+        id: 512,
+      },{
+        name: "Joe White Roasted Wheat Malt",
+        id: 513,
+      },{
+        name: "Joe White Wheat Malt",
+        id: 514,
+      },{
+        name: "Munton's Wheat Malt",
+        id: 515,
+      },{
+        name: "Simpsons Golden Naked Oats",
+        id: 516,
+      },{
+        name: "Weyermann Pale Wheat",
+        id: 517,
+      },{
+        name: "Weyermann Rye Malt",
+        id: 518,
+      }]
+    },{
+        name: "Other Grains",
+        id: 6,
+        data: [{
+          name: "Castle Abbey (Amber) Malted Barley",
+          id: 601,
+        },{
+          name: "Castle Acid Malt",
+          id: 602,
+        },{
+          name: "Castle Arôme Malted Barley",
+          id: 603,
+        },{
+          name: "Castle Biscuit Malted Barley",
+          id: 604,
+        },{
+          name: "Castle Black Malted Barley",
+          id: 605,
+        },{
+          name: "Castle Buckwheat Malt",
+          id: 606,
+        },{
+          name: "Castle Cafe",
+          id: 607,
+        },{
+          name: "Castle CaraGold (Cara 120) Malted Barley",
+          id: 608,
+        },{
+          name: "Castle Light Melanoidin Malted Barley",
+          id: 609,
+        },{
+          name: "Castle Melanoidin Malted",
+          id: 610,
+        },{
+          name: "Castle Viennat",
+          id: 611,
+        },{
+          name: "Crisp Naked Oat Malt",
+          id: 612,
+        },{
+          name: "Gladfield Gladiator Malt",
+          id: 613,
+        },{
+          name: "Joe White Amber Malt",
+          id: 614,
+        },{
+          name: "Simpsons Aromatic Barley Maltt",
+          id: 615,
+        },{
+          name: "Simpsons Imperial Malt",
+          id: 616,
+        },{
+          name: "Weyermann Acidulated Malt",
+          id: 617,
+        },{
+          name: "Weyermann Melanoiden",
+          id: 618,
+        }]
+      },{
+        name: "Smoked Grains",
+        id: 7,
+        data: [{
+          name: "Castle Peated Malted Barley",
+          id: 701,
+        },{
+          name: "Castle Smoked",
+          id: 702,
+        },{
+          name: "Castle Whisky Light",
+          id: 703,
+        },{
+          name: "Castle Whisky Malted",
+          id: 704,
+        },{
+          name: "Gladfield Manuka Smoke Malt",
+          id: 705,
+        },{
+          name: "Gladfield Mild Peat Smoked Malt",
+          id: 706,
+        },{
+          name: "Weyermann Smoked Malt",
+          id: 707,
+        }]
+    },{
+        name: "Unbranded Grains",
+        id: 8,
+        data: [{
+          name: "Peated",
+          id: 801,
+        },{
+          name: "Smoked",
+          id: 802,
+        },{
+          name: "Whisky",
+          id: 803,
+        },{
+          name: "Rye",
+          id: 804,
+        },{
+          name: "Amber",
+          id: 805,
+        },{
+          name: "Wheat",
+          id: 807,
+        },{
+          name: "Pale",
+          id: 808,
+        },{
+          name: "Malted",
+          id: 809,
+        },{
+          name: "Light Crystal",
+          id: 810,
+        },{
+          name: "Pilsner",
+          id: 811,
+        },{
+          name: "Lager",
+          id: 812,
+        },{
+          name: "Traditional Ale",
+          id: 813,
+        },{
+          name: "Pale Ale",
+          id: 814,
+        },{
+          name: "Munich",
+          id: 815,
+        },{
+          name: "Vienna",
+          id: 816,
+        },{
+          name: "American Pale Ale",
+          id: 817,
+        },{
+          name: "Roasted",
+          id: 818,
+        },{
+          name: "Chocolate",
+          id: 819,
+        },{
+          name: "Brown",
+          id: 820,
+        }]
+    }
+  ]
